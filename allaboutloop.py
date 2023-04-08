0946f896e2a0e28fb549f1a4e3edbe9bd7353f3c
komal jha <kjha4951@gmail.com>
2023-04-08 20:46:38 +0530

    star pattern

---
+++ allaboutloop.py
@@ -0,0 +1,22 @@
+
+# for i in range(10):
+#     print(i*' *')
+    
+#  * 
+#  * *
+#  * * *
+#  * * * *
+#  * * * * *
+#  * * * * * *
+#  * * * * * * *
+#  * * * * * * * *
+#  * * * * * * * * *
+
+
+for row in range(1,6):
+    for colomn in range(1,row+1):
+        print("*",end="")
+    print("\n")
+
+
+
