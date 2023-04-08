1d0c6addc19f0debcb0713f76e0dbe3cdabe4a08
komal jha <kjha4951@gmail.com>
2023-04-08 20:33:12 +0530

    Beginner

---
+++ start.py
@@ -0,0 +1,158 @@
+# in python we only use single and double quotes
+# print("Hello world!")
+
+
+# we use variable to temporary store data in memory
+
+
+
+
+
+# name =input("what is your name? ")
+# print("Hello  " +name)
+# we have three type of data in python
+# 10       numbers 
+# "komal"  strings
+# True     boolean
+
+
+# birth_year=int(input("enter your birth year: "))
+# age=2023-birth_year
+# print(age)
+
+
+
+# first=float(input(" first "))
+# second=int(input(" second "))
+# print(first+second)
+
+
+
+# ls=[i for i in range(100) if i%3==0]
+# print(ls)
+
+
+# course ="hi come with me its safe"
+# print(course.find("with"))
+# print(course.replace("come","4"))
+# print("come" in course)  cheking that this value is in the strin g or not
+
+
+# ls=[2,3,4,5,6]
+# ls[0]=9
+# print(ls)
+# x=10
+# x +=3
+# print(x)
+
+
+# x=10 +3*2
+# print(x)
+# x=(10+3)*2
+# print(x)
+# c=3 != 3
+# print(c)
+# these are the operators
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+
+
+# price=25
+# # print(price>10 and  price<30)
+# print(not price<10)
+
+
+#if statement
+# temperature=1
+# if temperature >30 :
+#     print("its hot day")
+#     print("drink plenty of water")
+# elif temperature>20:
+#     print("its a nice day")
+# elif temperature>10:
+#     print("its bit cold") 
+# else:
+#     print("its too cold")
+
+
+
+# weight=int(input("weight : "))
+# ask=input("(k)g or (l)bs")
+# if ask.upper() == "K":
+#         converted=weight/0.45
+#         print("the weight in lbs "+str(converted))
+# else:
+#        converted=weight*0.45
+#        print("the weight in kg " +str(converted ))
+   
+
+
+
+# i=1
+# while i<=10:
+#     print(i * " *")
+#     i +=1
+
+
+
+# names=[ 'koal','renu','pallavi','puja']
+# print(names[0:3])
+# print(names[-1])
+# names[0]="komal"
+# print(names)
+
+
+# numbers=[1,2,3,4,5]
+# numbers.append(7)
+# numbers.insert(0,"komal")
+# # numbers.remove(3)
+# # numbers.clear()
+# print(numbers)
+# print("komal" in numbers)
+# print(len(numbers))
+
+
+
+
+# numbers=[1,2,3,4,5,7]
+# for item in numbers:
+#     print(item)
+
+# numbers=[1,2,3,4,5,7]
+# i=0
+# while i<len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+
+
+# numbers=range(5)
+# print(numbers)
+
+
+
+# numbers = range(5)
+# for numbers in numbers:
+#   print(numbers)
+
+
+# numbers = range(5,10)
+# for numbers in numbers:
+#   print(numbers)
+
+
+# for numbers in range(5,10,3):
+#   print(numbers)
+
+
+
+tup=(1,2,3)
+print(tup.count(3))
+print(tup.index(2))
+
+
